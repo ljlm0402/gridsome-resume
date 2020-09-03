f5a7f4e2b76b762b84d19fe6643650e1780437ea
임경민 <ljlm0402@naver.com>
2020-09-03 19:39:20 +0900

    Fix Project, Tech Stack Styles

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -6,20 +6,23 @@
       <!-- 안랩 -->
       <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
         <div class="resume-content">
-          <h3 class="mb-0">Ahnlab (주)안랩</h3>
+          <h3 class="mb-0">Ahnlab .Inc (주)안랩</h3>
           <div class="subheading mb-3">ICM</div>
-          <p>사내 내부 서비스 개발</p>
-          <p>
-            <img class="resume-stack" src="@/assets/images/experience/typescript.png">
-            <img class="resume-stack" src="@/assets/images/experience/javascript.png">
-            <img class="resume-stack" src="@/assets/images/experience/nodejs.png">
-            <img class="resume-stack" src="@/assets/images/experience/vue.png">
-            <img class="resume-stack" src="@/assets/images/experience/sass.png">
-            <img class="resume-stack" src="@/assets/images/experience/postgresql.png">
-            <img class="resume-stack" src="@/assets/images/experience/mongodb.png">
-            <img class="resume-stack" src="@/assets/images/experience/docker.png">
-            <img class="resume-stack" src="@/assets/images/experience/kubernetes.png">
-          </p>
+          <p>사내 서비스 개발</p><br/>
+
+          <!-- 기술 -->
+          <div class="tech-box">
+            <img class="tech-image first" src="@/assets/images/experience/typescript.png">
+            <img class="tech-image" src="@/assets/images/experience/javascript.png">
+            <img class="tech-image" src="@/assets/images/experience/nodejs.png">
+            <img class="tech-image" src="@/assets/images/experience/vue.png">
+            <img class="tech-image" src="@/assets/images/experience/sass.png">
+            <img class="tech-image" src="@/assets/images/experience/postgresql.png">
+            <img class="tech-image" src="@/assets/images/experience/mongodb.png">
+            <img class="tech-image" src="@/assets/images/experience/docker.png">
+            <img class="tech-image" src="@/assets/images/experience/kubernetes.png">
+            <img class="tech-image" src="@/assets/images/experience/bitbucket.png">
+          </div>
         </div>
         <div class="resume-date text-md-right">
           <span class="text-primary">2020. 03 ~ 재직중</span>
@@ -31,31 +34,46 @@
         <div class="resume-content">
           <h3 class="mb-0">Fooding (주)열두달</h3>
           <div class="subheading mb-3">Fooding</div>
-          <p>단체음식 배송 플랫폼 서비스의 홈페이지 개발</p>
-          <p>
-            <img class="resume-stack" src="@/assets/images/experience/typescript.png">
-            <img class="resume-stack" src="@/assets/images/experience/javascript.png">
-            <img class="resume-stack" src="@/assets/images/experience/nodejs.png">
-            <img class="resume-stack" src="@/assets/images/experience/angular.png">
-            <img class="resume-stack" src="@/assets/images/experience/ionic.jpg">
-            <img class="resume-stack" src="@/assets/images/experience/sass.png">
-            <img class="resume-stack" src="@/assets/images/experience/mysql.png">
-            <img class="resume-stack" src="@/assets/images/experience/docker.png">
-            <img class="resume-stack" src="@/assets/images/experience/aws.png">
-            <img class="resume-stack" src="@/assets/images/experience/github.png">
-          </p>
-          <p>▼ 메인 홈페이지 <br/>
-            <iframe class="resume-movie" src="https://www.youtube.com/embed/jQ1YNnLlUKs"></iframe>
-          </p>
-          <p>▼ 관리자 홈페이지 <br/>
-            <iframe class="resume-movie" src="https://www.youtube.com/embed/jeXTYYE-4JU"></iframe>
-          </p>
-          <p>▼ 배송기사 앱 <br/>
-            <iframe class="resume-movie" src="https://www.youtube.com/embed/-IUGiugc8kM"></iframe>
-          </p>
-          <p>▼ 알림톡 서비스 <br/>
-          <iframe class="resume-movie" src="https://www.youtube.com/embed/hcjFL6LDWYQ"></iframe>
-          </p>
+          <p>단체음식 배송 플랫폼 서비스의 홈페이지 개발</p><br/>
+
+          <!-- 기술 -->
+          <div class="tech-box">
+            <img class="tech-image" src="@/assets/images/experience/typescript.png">
+            <img class="tech-image" src="@/assets/images/experience/javascript.png">
+            <img class="tech-image" src="@/assets/images/experience/nodejs.png">
+            <img class="tech-image" src="@/assets/images/experience/angular.png">
+            <img class="tech-image" src="@/assets/images/experience/ionic.png">
+            <img class="tech-image" src="@/assets/images/experience/sass.png">
+            <img class="tech-image" src="@/assets/images/experience/mysql.png">
+            <img class="tech-image" src="@/assets/images/experience/docker.png">
+            <img class="tech-image" src="@/assets/images/experience/aws.png">
+            <img class="tech-image" src="@/assets/images/experience/github.png">
+          </div>
+
+          <!-- 영상 -->
+            <div class="video-box">
+              <div class="video-container">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/jQ1YNnLlUKs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
+
+            <div class="video-box">
+              <div class="video-container">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/jeXTYYE-4JU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
+
+            <div class="video-box">
+              <div class="video-container">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/-IUGiugc8kM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
+
+            <div class="video-box">
+              <div class="video-container">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/hcjFL6LDWYQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
         </div>
         <div class="resume-date text-md-right">
           <span class="text-primary">2018. 10 ~ 2020. 03 (약 1년 5개월)</span>
@@ -63,27 +81,34 @@
       </div>
 
       <!-- 씨큐프라임 -->
-      <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
+      <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
         <div class="resume-content">
           <h3 class="mb-0">SECUPRIME (주)씨큐프라임</h3>
           <div class="subheading mb-3">E-MORDER KIOSK</div>
-          <p>POS와 무인주문결제기(키오스크)를 사용하여 통합관리 솔루션 개발</p>
-          <p>
-            <img class="resume-stack" src="@/assets/images/experience/javascript.png">
-            <img class="resume-stack" src="@/assets/images/experience/nodejs.png">
-            <img class="resume-stack" src="@/assets/images/experience/html.png">
-            <img class="resume-stack" src="@/assets/images/experience/css.png">
-            <img class="resume-stack" src="@/assets/images/experience/mariadb.png">
-            <img class="resume-stack" src="@/assets/images/experience/raspberry-pi.png">
-          </p>
-          <p>▼ 키오스크 화면 <br/>
-            <iframe class="resume-movie" src="https://www.youtube.com/embed/64V7ATPeeyM"></iframe>
-          </p>
+          <p>POS와 무인주문결제기(키오스크)를 사용하여 통합관리 솔루션 개발</p><br/>
+
+          <!-- 기술 -->
+          <div class="tech-box">
+            <img class="tech-image" src="@/assets/images/experience/javascript.png">
+            <img class="tech-image" src="@/assets/images/experience/nodejs.png">
+            <img class="tech-image" src="@/assets/images/experience/html.png">
+            <img class="tech-image" src="@/assets/images/experience/css.png">
+            <img class="tech-image" src="@/assets/images/experience/mariadb.png">
+            <img class="tech-image" src="@/assets/images/experience/raspberry-pi.png">
+          </div>
+
+          <!-- 영상 -->
+          <div class="video-box">
+              <div class="video-container">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/64V7ATPeeyM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
         </div>
         <div class="resume-date text-md-right">
           <span class="text-primary">2017. 12 ~ 2018. 10 (약 10개월)</span>
         </div>
       </div>
+
     </div>
   </section>
 </template>
