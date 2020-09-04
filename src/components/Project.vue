5368fdc1ec9a10f70ef14eaa1a19bbed0005a197
임경민 <ljlm0402@naver.com>
2020-09-04 20:17:21 +0900

    Revert "Fix Project, Tech Stack Styles"
    This reverts commit f5a7f4e2b76b762b84d19fe6643650e1780437ea.

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -6,36 +6,135 @@
       <!-- TypeScript Express Starter -->
       <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
         <div class="resume-content">
-          <a href="https://github.com/ljlm0402/typescript-express-starter">
-            <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=ljlm0402&repo=typescript-express-starter" />
-          </a>
+          <h3 class="mb-0">TypeScript Express Stater</h3>
+          <div class="subheading mb-3">Quick and Easy TypeScript Express Starter</div>
+          <div class="social-icons">
+            <a href="https://github.com/ljlm0402/typescript-express-starter" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'github']"/>
+            </a>
+            <a href="https://www.npmjs.com/package/typescript-express-starter" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'npm']"/>
+            </a>
+          </div>
+          <br />
+          <p>
+            <font-awesome :icon="['fas', 'download']"/> 3,111
+            <font-awesome :icon="['fas', 'star']"/> 94
+            <font-awesome :icon="['fas', 'share']"/> 20
+          </p>
+          <p>MIT</p>
         </div>
       </div>
 
       <!-- TypeScript Vue Starter -->
       <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
         <div class="resume-content">
-          <a href="https://github.com/ljlm0402/typescript-vue-starter">
-            <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=ljlm0402&repo=typescript-vue-starter" />
-          </a>
+          <h3 class="mb-0">TypeScript Vue Starter</h3>
+          <div class="subheading mb-3">Quick and Easy TypeScript Vue Starter</div>
+          <div class="social-icons">
+            <a href="https://github.com/ljlm0402/typescript-vue-starter" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'github']"/>
+            </a>
+            <a href="https://www.npmjs.com/package/typescript-vue-starter" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'npm']"/>
+            </a>
+          </div>
+          <br />
+          <p>
+            <font-awesome :icon="['fas', 'download']"/> 196
+            <font-awesome :icon="['fas', 'star']"/> 2
+            <font-awesome :icon="['fas', 'share']"/> 0
+          </p>
+          <p>MIT</p>
         </div>
       </div>
 
       <!-- Vuex State Storeage Sync -->
       <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
         <div class="resume-content">
-          <a href="https://github.com/ljlm0402/vuex-state-storage-sync">
-            <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=ljlm0402&repo=vuex-state-storage-sync" />
-          </a>
+          <h3 class="mb-0">Vuex State Storage Sync</h3>
+          <div class="subheading mb-3">Synchronization of vuex state and storage</div>
+          <div class="social-icons">
+            <a href="https://github.com/ljlm0402/vuex-state-storage-sync" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'github']"/>
+            </a>
+            <a href="https://www.npmjs.com/package/vuex-state-storage-sync" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'npm']"/>
+            </a>
+          </div>
+          <br />
+          <p>
+            <font-awesome :icon="['fas', 'download']"/> 110
+            <font-awesome :icon="['fas', 'star']"/> 0
+            <font-awesome :icon="['fas', 'share']"/> 0
+          </p>
+          <p>MIT</p>
         </div>
       </div>
+
+      <!-- Vue State Store Persist -->
+      <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div class="resume-content">
+          <h3 class="mb-0">Vue State Store Persist (🚧 Construction)</h3>
+          <div class="subheading mb-3">JavaScript Object Query Builder </div>
+          <div class="social-icons">
+            <a href="https://github.com/ljlm0402//vue-state-store-persist" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'github']"/>
+            </a> 
+            <a href="https://www.npmjs.com/package/vue-state-store-persist" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'npm']"/>
+            </a>
+          </div>
+          <br />
+          <p>
+            <font-awesome :icon="['fas', 'download']"/> 0
+            <font-awesome :icon="['fas', 'star']"/> 0
+            <font-awesome :icon="['fas', 'share']"/> 0
+          </p>
+      </div>
+    </div>
 
       <!-- TypeScript Express Gen -->
       <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
         <div class="resume-content">
-          <a href="https://github.com/ljlm0402/typescript-express-gen">
-            <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=ljlm0402&repo=typescript-express-gen" />
-          </a>
+          <h3 class="mb-0">TypeScript Express Gen (🚧 Construction)</h3>
+          <div class="subheading mb-3">Generate create based Typescript Express</div>
+          <div class="social-icons">
+            <a href="https://github.com/ljlm0402/typescript-express-gen" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'github']"/>
+            </a>
+            <a href="https://www.npmjs.com/package/typescript-express-gen" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'npm']"/>
+            </a>
+          </div>
+          <br />
+          <p>
+            <font-awesome :icon="['fas', 'download']"/> 0
+            <font-awesome :icon="['fas', 'star']"/> 0
+            <font-awesome :icon="['fas', 'share']"/> 0
+          </p>
+        </div>
+      </div>
+
+      <!-- Object Query Builder -->
+      <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
+        <div class="resume-content">
+          <h3 class="mb-0">Object Query Builder (🚧 Construction)</h3>
+          <div class="subheading mb-3">JavaScript Object Query Builder </div>
+          <div class="social-icons">
+            <a href="https://github.com/ljlm0402/object-querybuilder" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'github']"/>
+            </a>
+            <a href="https://www.npmjs.com/package/object-querybuilder" target="_blank" rel="noopener noreferrer">
+              <font-awesome :icon="['fab', 'npm']"/>
+            </a>
+          </div>
+          <br />
+          <p>
+            <font-awesome :icon="['fas', 'download']"/> 0
+            <font-awesome :icon="['fas', 'star']"/> 0
+            <font-awesome :icon="['fas', 'share']"/> 0
+          </p>
         </div>
       </div>
     </div>
