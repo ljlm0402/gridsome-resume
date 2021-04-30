65bb30a73f4d6325c5ecba847a8e5547eb112562
임경민 <ljlm0402@naver.com>
2021-04-30 10:03:17 +0900

    Communit
    - 실무코딩 추가

--- src/components/Community.vue
+++ src/components/Community.vue
@@ -32,6 +32,26 @@
           </a>
         </li>
       </ul>
+
+      <h2 class="mb-5"></h2>
+      <h3>실무코딩</h3>
+      <p>
+        실무에서 일어나는 다양한 코딩문제와 노하우를 공유하고 문제를 서로
+        해결해가는 모임으로 운영진으로 활동하고 있습니다.
+      </p>
+      <ul class="fa-ul mb-0">
+        <li>
+          <a
+            href="https://www.facebook.com/groups/smcoding/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="facebook link"
+          >
+            <font-awesome class="fa-li" :icon="['fab', 'facebook']" />FaceBook
+            Link
+          </a>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
